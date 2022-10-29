bf505753bb8ebc93365e71271709727f04fce800
volodya0 <volodauzuk02@gmail.com>
2022-10-29 23:37:43 +0300

    Permutational encoding implemented

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const encodeButton = document.getElementById("EncodeButton");
-const messageInput = document.getElementById("MessageInput");
-const stepInput = document.getElementById("StepInput");
-const encodeResultField = document.getElementById("EncodeResult");
+const alphabetUkr = " АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯ";
 
-encodeButton.addEventListener("click", onEncodeClick);
+const caesarDefaultValue =
+    "При використанні моноалфавітної заміни окремі букви відкритого тексту заміняються іншими буквами або числами або якимись іншими символами.";
+const caesarEncodeButton = document.querySelector(".Caesar #EncodeButton");
+const caesarMessageInput = document.querySelector(".Caesar #MessageInput");
+const caesarStepInput = document.querySelector(".Caesar #StepInput");
+const caesarEncodeResultField = document.querySelector(".Caesar #EncodeResult");
 
-function onEncodeClick() {
-    const message = messageInput.value;
-    const step = stepInput.value;
+caesarEncodeButton.addEventListener("click", onCaesarEncodeClick);
+caesarMessageInput.innerHTML = caesarDefaultValue;
+
+function onCaesarEncodeClick() {
+    const message = caesarMessageInput.value;
+    const step = caesarStepInput.value;
 
     const result = EncodeCaesar(message, step);
 
-    encodeResultField.innerText = result;
+    caesarEncodeResultField.innerText = result;
 }
-
-const alphabetUkr = " АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯ";
 
 function EncodeCaesar(message, step, _isDecoding = false) {
     let result = "";
 
     for (let i = 0; i < message.length; i++) {
-        const originalLetter = message[i];
-        const originalLetterIndex = alphabetUkr.indexOf(
-            originalLetter.toUpperCase()
-        );
+        const originalLetter = message[i].toUpperCase();
+        const originalLetterIndex = alphabetUkr.indexOf(originalLetter);
 
         if (originalLetterIndex === -1) {
             result += originalLetter;
@@ -36,4 +37,47 @@ function EncodeCaesar(message, step, _isDecoding = false) {
     }
 
     return result;
+}
+
+const permutationalDefaultValue =
+    "При використанні перестановочного шифру повідомлення записуєтся в горизонтальні рядки однакової довжини й наступне зчитування тексту стовпець за стовпцем, але не один по одному, а відповідно до деякої перестановки стовпців";
+const permutationalEncodeButton = document.querySelector(
+    ".Permutational #EncodeButton"
+);
+const permutationalMessageInput = document.querySelector(
+    ".Permutational #MessageInput"
+);
+const permutationalKeyInput = document.querySelector(
+    ".Permutational #StepInput"
+);
+const permutationalEncodeResultField = document.querySelector(
+    ".Permutational #EncodeResult"
+);
+
+permutationalEncodeButton.addEventListener("click", onPermutationalEncodeClick);
+permutationalMessageInput.innerHTML = permutationalDefaultValue;
+
+function onPermutationalEncodeClick() {
+    const message = permutationalMessageInput.value;
+    const key = permutationalKeyInput.value;
+
+    const result = EncodePermutational(message, key);
+
+    permutationalEncodeResultField.innerText = result;
+}
+
+function EncodePermutational(message, key, _isDecoding = false) {
+    const keyLength = key.length;
+    const columns = new Array(keyLength).fill(0).map((a) => []);
+
+    for (let i = 0; i < message.length; i++) {
+        const letter = message[i];
+        const columnIndex = i % keyLength;
+        columns[columnIndex].push(letter);
+    }
+
+    return columns
+        .reduce((res, arr) => (res += arr.join("")), "")
+        .replace(/ /g, "")
+        .toUpperCase();
 }
